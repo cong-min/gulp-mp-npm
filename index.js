1439821dbd856d87e2b2924d01ee9655cc2040d6
Cong Min <i@congm.in>
2020-03-18 22:53:57 +0800

    refactor: gulp-mp-npm

--- index.js
+++ index.js
@@ -5,184 +5,224 @@ const through = require('through2');
 const vfs = require('vinyl-fs');
 const changed = require('gulp-changed');
 const logger = require('fancy-log');
+const treeToList = require('tree-to-list');
+const checkPackage = require('./lib/checkPackage');
 const lookupComponents = require('./lib/lookupComponents');
 const lookupDependencies = require('./lib/lookupDependencies');
 // const rewriteModulesPath = require('./lib/rewriteModulePath');
-const getNodeModulesPath = require('./lib/utils/getNodeModulesPath');
-const getPackageName = require('./lib/utils/getPackageName');
 
-const defaultNpmOutput = 'miniprogram_npm'; // 小程序官方方案默认输出路径
+const defaultNpmDirname = 'miniprogram_npm'; // 小程序官方方案默认输出路径
+const cwd = process.cwd();
 
-/** extractComps
- * 提取小程序组件专用 npm 包, 将依赖文件追加至 stream 流中
- */
-function extractComps(npmOutput) {
-    const extracted = {}; // 已提取的组件文件夹路径
-
-    async function transform(file, enc, next) {
-        const stream = this;
-        if (file.isNull()) return next(null, file);
-
-        // 如果不是 json 文件, 则跳过
-        if (file.extname !== '.json') return next(null, file);
-
-        // 如果是 json 文件, 尝试读取并寻找其声明的小程序组件专用 npm 包
-        const fileContent = String(file.contents); // 获取文件内容
-        // 找出组件依赖树
-        const compTree = await lookupComponents(fileContent, file.path);
-        // 展开树并去重处理为映射
-        const npmComponents = lookupComponents.treeToMap(compTree);
-        // 展开专用包构建路径列表
-        const pathGlobs = Object.keys(npmComponents)
-            .filter(e => !extracted[e])
-            .map(compPath => path.join(compPath, '**'));
-
-        if (!pathGlobs.length) return next(null, file);
-
-        return vfs.src(pathGlobs)
-            // 修改 depFile.base 及 path, 去除 buildPath 对应的构建文件夹
-            .pipe(through.obj((depFile, depEnc, depNext) => {
-                if (depFile.isNull()) return depNext(null, depFile);
-
-                const originPath = depFile.path;
-                const nodeModulesPath = getNodeModulesPath(originPath); // 找到所在的 node_modules 文件夹
-                const packageName = getPackageName(originPath); // 获取包名
-
-                // 找到匹配的组件信息
-                const componentPath = Object.keys(npmComponents)
-                    .find(compPath => originPath.indexOf(compPath) === 0);
-                if (!componentPath) return depNext(null, depFile);
-                const { buildPath, expression } = npmComponents[componentPath];
-                // 小程序组件 npm 专用包需要修改路径, 将 buildPath 文件夹修改为依赖的根目录
-                const relativePath = path.relative(buildPath, originPath); // 文件在构建目录下的子路径
-                const outputPath = path.join(npmOutput, packageName, relativePath);
-
-                depFile.path = path.join(nodeModulesPath, outputPath);
-                depFile.base = nodeModulesPath;
-                depFile.packageName = packageName;
-                depFile.expression = expression; // 引用时的表达式
-
-                return depNext(null, depFile);
-            }))
-            .on('data', (data) => {
-                // stream.push 追加文件
-                stream.push(data);
-            })
-            .on('finish', () => {
-                // 打印日志
-                Object.keys(npmComponents).forEach((componentPath) => {
-                    const { expression } = npmComponents[componentPath];
-                    if (!extracted[componentPath]) {
-                        extracted[componentPath] = true;
-                        // 打印出根首层依赖的日志
-                        if (compTree[componentPath]) logger.info('[mp-npm]', `Extracted \`${expression}\``);
-                    }
-                });
-                next(null, file);
-            })
-            .on('error', next);
-    }
-
-    return through.obj(transform);
-}
-
-/** extractDeps
- * 提取文件依赖树, 将依赖文件追加至 stream 流中
- */
-function extractDeps(npmOutput) {
-    const extracted = []; // 已提取的依赖名
-
-    async function transform(file, enc, next) {
-        const stream = this;
-        if (file.isNull()) return next(null, file);
-
-        // 找出文件依赖树
-        const tree = await lookupDependencies(file.path);
-        // 展开树并去重处理为映射
-        const depMap = lookupDependencies.treeToMap(tree);
-        // 展开依赖文件路径列表
-        const depPaths = Object.keys(depMap).filter(e => !extracted[e]);
-
-        if (!depPaths.length) return next(null, file);
-
-        return vfs.src(depPaths)
-            // 修改 depFile.base | path
-            .pipe(through.obj((depFile, depEnc, depNext) => {
-                if (depFile.isNull()) return depNext(null, depFile);
-
-                const originPath = depFile.path;
-                const nodeModulesPath = getNodeModulesPath(originPath); // 找到所在的 node_modules 文件夹
-
-                const matchedDep = depMap[originPath]; // 找到匹配依赖信息
-                if (!matchedDep) return depNext(null, depFile);
-                const { name: packageName, expression, packagePath } = matchedDep;
-                // 通过相对路径获取输出路径
-                const relativePath = path.relative(packagePath, originPath);
-                const outputPath = path.join(npmOutput, packageName, relativePath);
-
-                depFile.path = path.join(nodeModulesPath, outputPath);
-                depFile.base = nodeModulesPath;
-                depFile.packageName = packageName;
-                depFile.expression = expression; // 引用时的表达式
-
-                // 记录提取
-                if (!extracted[depFile.path]) {
-                    extracted[depFile.path] = true;
-                    // 打印出根首层依赖的日志
-                    if (tree[originPath]) logger.info('[mp-npm]', `Extracted \`${expression}\``);
-                }
-
-                return depNext(null, depFile);
-            }))
-            .on('data', (data) => {
-                // stream.push 追加文件
-                stream.push(data);
-            })
-            .on('finish', () => next(null, file))
-            .on('error', next);
-    }
-    return through.obj(transform);
-}
-
-/** adjustPath
- * 调整文件及引用路径, 保证正确的输出
- */
-function adjustPath(npmOutput) {
-    async function transform(file, enc, next) {
-        if (file.isNull()) return next(); // 不输出空文件
-        const { packageName, expression } = file;
-
-        // 过滤掉非 node_modules 的文件
-        if (!getNodeModulesPath(file.path) || !packageName) return next(); // 不输出非 npm 文件
-
-        /* 不同方案下的文件路径修改 */
-        // 如果是导出文件夹为官方小程序 miniprogram_npm 方案
-        if (npmOutput === defaultNpmOutput) {
-            // 那么当引入依赖的是模块主入口时，需将入口重写至 index.js
-            if (packageName === expression) {
-                file.path = path.join(file.base, npmOutput, packageName, `index${path.extname(file.path) || '.js'}`);
-            }
-        } else {
-            // 否则需将原代码中引用依赖的表达式路径加上 npmOutput
-            // let fileContent = String(file.contents); // 获取文件内容
-            // fileContent = rewriteModulesPath(fileContent, npmOutput);
-            // file.contents = Buffer.from(fileContent);
-        }
-
-        return next(null, file);
-    }
-
-    return through.obj(transform);
-}
+// 所有依赖包列表
+let pkgList = {};
+// 小程序专用 npm 依赖包名与构建路径的映射, 将作为 resolve 时的 alias
+const mpPkgMathMap = {};
+// 是否已初始化
+let inited = false;
 
 /**
  * gulp-mp-npm
  */
-function mpNpm(destPath, npmOutput = defaultNpmOutput, options = {}) {
+module.exports = function mpNpm(destPath, npmDirname = defaultNpmDirname, options = {}) {
+    /** init
+     * 初始化
+     */
+    function init() {
+        async function transform(file, enc, next) {
+            if (!inited) {
+                // 找出所有依赖包
+                pkgList = await checkPackage.checkAllPkgs(cwd);
+                // 筛选出小程序专用 npm 依赖包
+                Object.keys(pkgList).forEach(pkgName => {
+                    const pkg = pkgList[pkgName];
+                    if (pkg.isMiniprogramPkg && pkg.buildPath) {
+                        mpPkgMathMap[pkgName] = pkg.buildPath;
+                    }
+                });
+                inited = true;
+            }
+            next(null, file);
+        }
+        return through.obj(transform);
+    }
+
+    /** extractComps
+     * 提取小程序专用 npm 依赖包, 将依赖文件追加至 stream 流中
+     */
+    function extractComps() {
+        const extracted = {}; // 已提取的组件文件夹路径
+
+        async function transform(file, enc, next) {
+            const stream = this;
+            if (file.isNull()) return next(null, file);
+
+            // 如果不是 json 文件, 则跳过
+            if (file.extname !== '.json') return next(null, file);
+
+            const fileContent = String(file.contents); // 获取文件内容
+            // 找出小程序组件依赖树
+            const compTree = await lookupComponents(fileContent, file.path, {
+                alias: mpPkgMathMap
+            });
+            // 展开树并去重处理为映射
+            const npmComponents = treeToList(compTree, 'tree');
+            // 展开依赖包构建路径列表
+            const pathGlobs = Object.keys(npmComponents)
+                .filter(e => !extracted[e])
+                .map(compPath => path.join(compPath, '**'));
+
+            if (!pathGlobs.length) return next(null, file);
+
+            return lead(vfs.src(pathGlobs)
+                // 添加信息
+                .pipe(through.obj((depFile, depEnc, depNext) => {
+                    if (depFile.isNull()) return depNext(null, depFile);
+
+                    const originPath = depFile.path;
+                    const { packageName } = checkPackage.resolveDepFile(originPath);
+                    if (!packageName) return depNext(null, depFile);
+
+                    depFile.packageName = packageName;
+
+                    // stream.push 追加文件
+                    stream.push(depFile);
+                    return depNext(null, depFile);
+                }))
+                .on('finish', () => {
+                    // 打印日志
+                    Object.keys(npmComponents).forEach((componentPath) => {
+                        const { moduleId } = npmComponents[componentPath];
+                        // 记录提取
+                        if (!extracted[componentPath]) {
+                            extracted[componentPath] = true;
+                            // 打印出根首层依赖的日志
+                            if (compTree[componentPath]) logger.info('[mp-npm]', `Extracted \`${moduleId}\``);
+                        }
+                    });
+                    next(null, file);
+                })
+                .on('error', next));
+        }
+
+        return through.obj(transform);
+    }
+
+    /** extractDeps
+     * 提取文件依赖树, 将依赖文件追加至 stream 流中
+     */
+    function extractDeps() {
+        const extracted = []; // 已提取的依赖名
+
+        async function transform(file, enc, next) {
+            const stream = this;
+            if (file.isNull()) return next(null, file);
+
+            // 找出文件依赖树
+            const tree = await lookupDependencies(file.path, {
+                alias: mpPkgMathMap
+            });
+            // 展开树并去重处理为映射
+            const depMap = treeToList(tree, 'tree');
+            // 展开依赖文件路径列表
+            const depPaths = Object.keys(depMap).filter(e => !extracted[e]);
+
+            if (!depPaths.length) return next(null, file);
+
+            return lead(vfs.src(depPaths)
+                // 添加信息
+                .pipe(through.obj((depFile, depEnc, depNext) => {
+                    if (depFile.isNull()) return depNext(null, depFile);
+
+                    const originPath = depFile.path;
+
+                    const matchedDep = depMap[originPath]; // 找到匹配依赖信息
+                    if (!matchedDep) return depNext(null, depFile);
+
+                    const { name: packageName, moduleId } = matchedDep;
+                    depFile.packageName = packageName;
+                    depFile.moduleId = moduleId;
+
+                    // 记录提取
+                    if (!extracted[depFile.path]) {
+                        extracted[depFile.path] = true;
+                        // 打印出根首层依赖的日志
+                        if (tree[originPath]) logger.info('[mp-npm]', `Extracted \`${moduleId}\``);
+                    }
+
+                    // stream.push 追加文件
+                    stream.push(depFile);
+                    return depNext(null, depFile);
+                }))
+                .on('finish', () => next(null, file))
+                .on('error', next));
+        }
+        return through.obj(transform);
+    }
+
+    /** adjustPath
+     * 调整文件及引用路径, 保证正确的输出
+     */
+    function adjustPath() {
+        async function transform(file, enc, next) {
+            if (file.isNull()) return next(); // 不输出空文件
+
+            /* 调整 depFile.base 及 path */
+            const { packageName, moduleId } = file;
+
+            // 去掉小程序专用 npm 依赖包路径中的 buildPath 部分
+            let filepath = file.path;
+            Object.keys(mpPkgMathMap).forEach((pkgName) => {
+                const pkg = pkgList[pkgName];
+                // build 相对路径
+                const buildRelativePath = pkg.isMiniprogramPkg
+                    ? path.relative(pkg.path, pkg.buildPath) : '';
+                    // 替换路径
+                filepath = filepath.replace(
+                    path.join('node_modules', pkgName, buildRelativePath),
+                    path.join('node_modules', pkgName)
+                );
+            });
+
+            // 以 node_modules 分割路径
+            const separator = '/node_modules/';
+            const pathSplit = filepath.replace(/\\/g, '/').split(separator);
+            if (!packageName || pathSplit.length === 1) return next(); // 非 npm 文件
+
+            // 取 pathSplit[0] 作为 path.base 用于 dest 替换
+            file.base = pathSplit[0];
+            // 以 npmDirname 替换 node_modules 将路径拼接
+            file.path = pathSplit.join(`/${npmDirname}/`);
+
+            /* 不同方案下的文件路径修改 */
+            // 如果是导出文件夹为官方小程序 miniprogram_npm 方案
+            if (npmDirname === defaultNpmDirname) {
+                // 那么当引入依赖的是模块主入口时，需将入口重写至 index.js
+                if (packageName === moduleId) {
+                    pathSplit[pathSplit.length - 1] = path.join(packageName, 'index.js');
+                    file.path = pathSplit.join(`/${npmDirname}/`);
+                }
+            } else {
+                // 否则需将原代码中引用依赖的表达式路径加上 npmOutput
+                // let fileContent = String(file.contents); // 获取文件内容
+                // fileContent = rewriteModulesPath(fileContent, npmOutput);
+                // file.contents = Buffer.from(fileContent);
+            }
+
+            return next(null, file);
+        }
+
+        return through.obj(transform);
+    }
+
+    /**
+     * start
+     */
     let pipeline = [
-        extractComps(npmOutput),
-        extractDeps(npmOutput),
-        adjustPath(npmOutput),
+        init(),
+        extractComps(),
+        extractDeps(),
+        adjustPath(),
     ];
 
     if (destPath) {
@@ -193,6 +233,4 @@ function mpNpm(destPath, npmOutput = defaultNpmOutput, options = {}) {
     }
 
     return lead(pumpify.obj(pipeline));
-}
-
-module.exports = mpNpm;
+};
