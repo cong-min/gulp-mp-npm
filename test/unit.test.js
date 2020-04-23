3ab5ea7b2ab27b83e8509fec94216b87746187eb
Cong Min <i@congm.in>
2020-04-23 19:15:39 +0800

    test(unit): adjust

--- test/unit.test.js
+++ test/unit.test.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const gulp = require('gulp');
 const glob = require('glob');
 const diff = require('lodash/difference')
-const jestDiff = require('jest-diff').default;
 const utils = require('./utils');
 jest.mock('console');
 const mpNpm = require('../index');
@@ -24,73 +23,37 @@ describe('单元测试', () => {
         done();
     });
 
-    // 测试输出
-    function testOuput(input, output, done, options = {}) {
-        const actualFiles = []; // 实际文件目录结构
-        const expectFiles = glob.sync(`${output}/**`, { cwd: unitExpected, absolute: true, nodir: true }); // 预期文件目录结构
-        gulp.src(input, { cwd: unitFixtures, nodir: true })
-            .pipe(mpNpm(options.mpNpmOptions))
-            // .pipe(gulp.dest(path.join(unitExpected, output)))
-            .on('error', done)
-            .on('data', (file) => {
-                expect(file).not.toBeNil();
-                expect(file.path).not.toBeNil();
-                expect(file.base).not.toBeNil();
-                expect(file.contents).not.toBeNil();
-                // 找到预期文件
-                const expectPath = path.join(unitExpected, output, file.path.replace(file.base, ''));
-                expect(fs.existsSync(expectPath) ? expectPath : undefined)
-                    .toBe(expectPath);
-                // 文件内容是否符合预期
-                const actualContent = utils.normaliseEOL(file.contents)
-                const expectContent = utils.normaliseEOL(fs.readFileSync(expectPath, 'utf8'));
-                if (actualContent.length > 5000 || expectContent.length > 5000) {
-                    expect(actualContent.length).toBe(expectContent.length);
-                } else {
-                    expect(actualContent).toBe(expectContent);
-                }
-                actualFiles.push(expectPath);
-            })
-            .on('end', () => {
-                // 实际比预期多出的文件
-                expect(diff(actualFiles, expectFiles)).toBeEmpty();
-                // 实际比预期缺少的文件
-                expect(diff(expectFiles, actualFiles)).toBeEmpty();
-                done();
-            })
-    }
-
     test('js 中引入普通 npm 依赖', (done) => {
-        testOuput('import-normal-dep.js', 'import-normal-dep.js/', done);
+        testUnitCase('import-normal-dep.js', 'import-normal-dep.js/', done);
     });
 
     test('wxss 中引入普通 npm 依赖', (done) => {
-        testOuput('import-normal-dep.wxss', 'import-normal-dep.wxss/', done);
+        testUnitCase('import-normal-dep.wxss', 'import-normal-dep.wxss/', done);
     });
 
     test('js 中引入小程序专用 npm 依赖', (done) => {
-        testOuput('import-special-dep.js', 'import-special-dep.js/', done);
+        testUnitCase('import-special-dep.js', 'import-special-dep.js/', done);
     });
 
     test('json 中引入小程序专用 npm 组件', (done) => {
-        testOuput('import-special-dep.json', 'import-special-dep.json/', done);
+        testUnitCase('import-special-dep.json', 'import-special-dep.json/', done);
     });
 
     test('wxss 中引入小程序专用 npm 组件', (done) => {
-        testOuput('import-special-dep.wxss', 'import-special-dep.wxss/', done);
+        testUnitCase('import-special-dep.wxss', 'import-special-dep.wxss/', done);
     });
 
     test('引入多重深层 npm 依赖及带命名空间的 npm 依赖', (done) => {
-        testOuput('import-deep-scope-dep.js', 'import-deep-scope-dep.js/', done);
+        testUnitCase('import-deep-scope-dep.js', 'import-deep-scope-dep.js/', done);
     });
 
     test('使用 require 进行依赖引入', (done) => {
-        testOuput('require-dep.js', 'require-dep.js/', done);
+        testUnitCase('require-dep.js', 'require-dep.js/', done);
     });
 
     test('自定义 npmDirname 提取文件夹', (done) => {
         const mpNpmOptions = { npmDirname: 'dist' };
-        testOuput([
+        testUnitCase([
             'import-normal-dep.js',
             'import-normal-dep.wxss',
             'import-special-dep.js',
@@ -102,3 +65,40 @@ describe('单元测试', () => {
     });
 
 });
+
+// 测试单元用例
+function testUnitCase(input, output, done, options = {}) {
+    const actualFiles = []; // 实际文件目录结构
+    const expectFiles = glob.sync(`${output}/**`, { cwd: unitExpected, base: unitExpected, absolute: true, nodir: true }); // 预期文件目录结构
+    gulp.src(input, { cwd: unitFixtures, base: unitFixtures, nodir: true })
+        .pipe(mpNpm(options.mpNpmOptions))
+        // .pipe(gulp.dest(path.join(unitExpected, output)))
+        .on('error', done)
+        .on('data', (file) => {
+            expect(file).not.toBeNil();
+            expect(file.path).not.toBeNil();
+            expect(file.base).not.toBeNil();
+            expect(file.contents).not.toBeNil();
+            // 找到预期文件
+            const relativePath = file.relative || path.relative(file.base, file.path);
+            const expectPath = path.resolve(path.join(unitExpected, output), relativePath);
+            expect(fs.existsSync(expectPath) ? expectPath : undefined)
+                .toBe(expectPath);
+            // 文件内容是否符合预期
+            const actualContent = utils.normaliseEOL(file.contents)
+            const expectContent = utils.normaliseEOL(fs.readFileSync(expectPath, 'utf8'));
+            if (actualContent.length > 5000 || expectContent.length > 5000) {
+                expect(actualContent.length).toBe(expectContent.length);
+            } else {
+                expect(actualContent).toBe(expectContent);
+            }
+            actualFiles.push(expectPath);
+        })
+        .on('end', () => {
+            // 实际比预期多出的文件
+            expect(diff(actualFiles, expectFiles)).toBeEmpty();
+            // 实际比预期缺少的文件
+            expect(diff(expectFiles, actualFiles)).toBeEmpty();
+            done();
+        });
+}
